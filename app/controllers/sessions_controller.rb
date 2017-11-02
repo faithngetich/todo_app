9aba0e61f761771978feb30b41b9090240b2e7c0
faithngetich <faith.ngetich@andela.com>
2017-11-02 17:20:14 +0300

    refactor signup-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,22 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:email])
     # user exists and enters correct password
-    if user && user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       # Save user id inside the browser cookie.
       # Keeps user logged in as they navigate through the website
-      session[:user_id] = user_id
-      redirects_to '/'
+      session[:user_id] = @user.id
+      redirect_to '/'
     else
       # send them back to login
-      redirects_to '/login'
+      flash.now[:error] = 'Invalid email/password combination.'
+      render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirects_to '/login'
+    render 'new'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   # second will
   # receive the form and create a user in our database using the data given to
   # us by the user.
-  get '/signup' => 'users#new'
+  get '/signup' => 'users#new', as: :signup
   post '/users' => 'users#create'
 
   # Shows a users login form, logging them in, and logging them out
   get 'login' => 'sessions#new'
-  get 'login' => 'sessions#create'
+  post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
 end
